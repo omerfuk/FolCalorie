ff1a585660e855e39f0d59ac4d68b00bb31f401d
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-28 07:32:05 +0300

    Initial Commit

---
+++ FolCalorie/FolCalorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolCalorieApp.swift
+//  FolCalorie
+//
+//  Created by Ömer Faruk Kılıçaslan on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FolCalorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
